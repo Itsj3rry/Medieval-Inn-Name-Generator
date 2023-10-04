b19eb33a4572a5ff2ae9819dbb952c317cf59759
Itsj3rry <120964427+Itsj3rry@users.noreply.github.com>
2023-10-04 18:38:59 -0500

    Add files via upload

---
+++ inn-name.py
@@ -0,0 +1,8 @@
+import random
+inn_adj = ['Orange','Confused','Suspicious','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio']
+inn_noun = ['Horse','Beaver','Its_j3rry','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean']
+inn_type = ['Inn','Tavern','Pub']
+adj=(random.choice(inn_adj))
+noun=(random.choice(inn_noun))
+type=(random.choice(inn_type))
+print(adj,noun,type)
