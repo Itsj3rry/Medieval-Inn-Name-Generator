214608b0fd3f084f85830c699394e98a6fd8e437
Itsj3rry <120964427+Itsj3rry@users.noreply.github.com>
2023-10-04 19:42:48 -0500

    Added 'The' to desc.

--- inn-name.py
+++ inn-name.py
@@ -1,9 +1,12 @@
 import random
-inn_adj = ['Orange','Confused','Suspicious','Emo','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio']
-inn_noun = ['Horse','Beaver','Its_j3rry','Feces','Capybara','Emu''I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean']
-inn_type = ['Inn','Tavern','Pub','The']
+inn_adj = ['Orange','Confused','Suspicious','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio','Emo']
+inn_noun = ['Horse','Beaver','Its_j3rry','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean','Emu']
+inn_type = ['Inn','Tavern','Pub','The','The']
 adj=(random.choice(inn_adj))
 noun=(random.choice(inn_noun))
 type=(random.choice(inn_type))
-if type='the' print(type)
-print(adj,noun,type)
+the='The'
+if type == the:
+ print(type,adj,noun)
+else:
+ print(adj,noun,type)
