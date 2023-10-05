d1e246486d155974ee14840216f5da1dd18b6e9c
Itsj3rry <120964427+Itsj3rry@users.noreply.github.com>
2023-10-04 19:44:08 -0500

    Update inn-name.py

--- inn-name.py
+++ inn-name.py
@@ -1,6 +1,6 @@
 import random
 inn_adj = ['Orange','Confused','Suspicious','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio','Emo']
-inn_noun = ['Horse','Beaver','Its_j3rry','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean','Emu']
+inn_noun = ['Horse','Beaver','Jerry','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean','Emu']
 inn_type = ['Inn','Tavern','Pub','The','The']
 adj=(random.choice(inn_adj))
 noun=(random.choice(inn_noun))
