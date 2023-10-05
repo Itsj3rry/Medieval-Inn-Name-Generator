b92716b7bd83d92f76f3a5a8e39da28e4f94949c
Itsj3rry <120964427+Itsj3rry@users.noreply.github.com>
2023-10-04 19:52:23 -0500

    Update inn-name.py

--- inn-name.py
+++ inn-name.py
@@ -1,7 +1,9 @@
 import random
-inn_adj = ['Orange','Confused','Suspicious','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio','Emo']
-inn_noun = ['Horse','Beaver','Jerry','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean','Emu']
+inn_adj = ['Orange','Emo','Confused','Suspicious','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio']
+inn_noun = ['Horse','Emu','Beaver','Insurance','Jerry','Alpaca','Wombat','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean']
 inn_type = ['Inn','Tavern','Pub','The','The']
+print ("The Medieval Inn Name Generator")
+print ("Has decided your inn should be named...")
 adj=(random.choice(inn_adj))
 noun=(random.choice(inn_noun))
 type=(random.choice(inn_type))
