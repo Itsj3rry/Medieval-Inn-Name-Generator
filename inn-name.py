c1cb62019b8595d5313dc533b8ec32c5ec00a8cb
Itsj3rry <120964427+Itsj3rry@users.noreply.github.com>
2023-10-04 19:26:15 -0500

    Update inn-name.py

--- inn-name.py
+++ inn-name.py
@@ -1,8 +1,9 @@
 import random
-inn_adj = ['Orange','Confused','Suspicious','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio']
-inn_noun = ['Horse','Beaver','Its_j3rry','Feces','I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean']
-inn_type = ['Inn','Tavern','Pub']
+inn_adj = ['Orange','Confused','Suspicious','Emo','Drunken','One-Legged','Cheesey','Stumbling','Sleep-Deprived Insommniac','Cooked','Prancing','Depressed','Enraged','Only In Ohio']
+inn_noun = ['Horse','Beaver','Its_j3rry','Feces','Capybara','Emu''I ran out of ideas :(','Eagle','Pony','Hummingbird','Drone','Baked Beans','Bean']
+inn_type = ['Inn','Tavern','Pub','The']
 adj=(random.choice(inn_adj))
 noun=(random.choice(inn_noun))
 type=(random.choice(inn_type))
+if type='the' print(type)
 print(adj,noun,type)
